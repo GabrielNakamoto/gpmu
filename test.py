676aa3801218e104aa6008970ceb093f1474f9f1
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-04-26 15:18:31 -0400

    work

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-from gpmu import load_kernels
+from gpmu import SIMTCore, Dim3, load_kernels
 
-kernels = load_kernels(open("test/kernel.ptx", 'r').read())
+kernels = load_kernels(open("test/gemm.ptx", 'r').read())
+gpu = SIMTCore()
+
+gpu(next(iter(kernels.values())), Dim3(1), Dim3(256), 1, 2, 3, 4, 5, 6)
